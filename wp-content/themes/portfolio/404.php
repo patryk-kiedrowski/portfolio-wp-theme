aed51052967b98963305f1673c8fc6f41b83154b
patryk-kiedrowski <patkiedr@gmail.com>
2020-11-08 11:51:43 +0100

    feat(404): created 404 page

---
+++ wp-content/themes/portfolio/404.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+
+<main class="subpage">
+  <section class="section-wrapper blog-list-wrapper">
+    <div class="section blog-list">
+      <header>
+        <h1 class="section__heading blog-list__heading">Strona nie istnieje [&nbsp;<span class="primary-text">404</span>&nbsp;]</h1>
+      </header>
+
+      <div id="content" class="wysiwyg">
+        <p>
+          Strona, której szukasz, nie istnieje.
+        </p>
+        <p>&nbsp;</p>
+      </div>
+
+      <div class="btn-container">
+        <a href="#" class="btn btn--primary btn--full">Strona główna</a>
+        <a href="#" class="btn btn--primary btn--outline">Blog</a>
+      </div>
+    </div>
+  </section>
+</main>
+
+<?php get_footer(); ?>
